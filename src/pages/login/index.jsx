635314a587f1ecdc043f0c84d82f9871db700fd9
Rou <1660522534@qq.com>
2019-06-21 21:02:22 +0800

    完成login页面的表单value获取功能

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -7,6 +7,30 @@ import './index.less';
 const Item = Form.Item;
 
 class Login extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((error,values) => {
+      if(!error){
+        const { username, password } = values;
+        console.log(username, password);
+      }else {
+        console.log(error);
+      }
+    })
+  };
+
+  validator(rule,value,callback) {
+    const name = rule.fullField === 'password'?'密码':'用户名';
+    if(value.length < 4){
+      callback(`${name}的长度必须大于4位`)
+    }else if(value.length>11){
+      callback(`${name}的长度必须小于11位`)
+    }else if(!/^[a-zA-Z_0-9]+$/.test(value)){
+      callback(`${name}只能包括字母、数字、下划线`)
+    }else {
+      callback()
+    }
+  };
   render() {
     const { getFieldDecorator } = this.props.form;
     return <div className="login">
@@ -16,12 +40,20 @@ class Login extends Component {
       </header>
       <section className="login-page">
         <h2>用户登录</h2>
-        <Form className="form-con">
+        <Form className="form-con" onSubmit={this.handleSubmit}>
           <Item>
             {
               getFieldDecorator(
-              'username',
-
+                'username',
+                /*{rules : [
+                    {max:11, message:'用户名长度不可超过11位'},
+                    {min:4, message:'用户名长度不可低于4位'},
+                    {required:true, message:'必须填写用户名'},
+                    {pattern:/^[a-zA-Z_0-9]+$/, message:'用户名只能包括字母、数字、下划线'}
+                 ]}*/
+                {rules : [
+                  {validator : this.validator}
+                ]}
               )(
                 <Input
                   prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -31,11 +63,21 @@ class Login extends Component {
             }
           </Item>
           <Item>
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="请输入密码"
-              className="input-con"
-            />
+            {
+              getFieldDecorator(
+                'password',
+                {rules : [
+                  {validator : this.validator}
+                ]}
+              )(
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  placeholder="请输入密码"
+                  className="input-con"
+                />
+              )
+            }
+
           </Item>
           <Item>
             <Button type="primary" htmlType="submit" className="input-btn">
