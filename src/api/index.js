0a50eb726d44e25445d3b2bf099c0ce8ecbc5c37
Rou <1660522534@qq.com>
2019-06-24 21:30:20 +0800

    完成category的部分交互

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,13 @@ import ajax from './ajax';
 import jsonp from 'jsonp';
 import { message } from 'antd';
 
+//用户登录
 export const reqUser = (username, password) => ajax('/login',{username,password},'post');
+
+//用户验证
 export const userConfirm = (id) => ajax('/main/confirm',{id},'post');
+
+//请求天气
 export const weatherMsg = function() {
   return new Promise((resolve) => {
     jsonp('http://api.map.baidu.com/telematics/v3/weather?location=深圳&output=json&ak=3p49MVra6urFRGOT9s8UBWr2', {}, function(err, data) {
@@ -20,3 +25,6 @@ export const weatherMsg = function() {
     })
   })
 }
+
+//category组件的数据请求
+export const reqCategoryDate = (parentId) => ajax('/manage/category/info',{parentId})
--- src/pages/category/index.jsx
+++ src/pages/category/index.jsx
@@ -1,9 +1,83 @@
 import React, { Component } from 'react';
+import { Card, Button, Icon, Table } from 'antd';
+
+import DefButton from '../../component/defined-button';
+import './index.less';
+import { reqCategoryDate } from '../../api'
+
 
 export default class Category extends Component {
+  state = {
+    categoryData : []
+  }
+
+  async componentDidMount(){
+    const result = await reqCategoryDate('0');
+    console.log(result);
+    if (result) {
+      this.setState({
+        categoryData : result
+      })
+    }
+
+  }
+
+
   render() {
-    return <div>
-      Category
-    </div>;
+
+    const columns = [
+      {
+        title: '品类名称',
+        dataIndex: 'name',
+
+      },
+      {
+        title: '操作',
+        dataIndex: 'operation',
+        className: 'left-operation',
+        render: () => {
+          return <div>
+            <DefButton>修改名称</DefButton>
+            <DefButton>查看子品类</DefButton>
+          </div>
+
+        },
+      },
+
+    ];
+
+    /*const data = [
+      {
+        key: '1',
+        name: '手机',
+
+      },
+      {
+        key: '2',
+        name: '电脑',
+      },
+      {
+        key: '3',
+        name: '平板',
+      },
+      {
+        key: '4',
+        name: '耳机',
+      },
+    ];*/
+
+    return <Card title="一级分类列表" extra={<Button type="primary"><Icon type="plus" />添加品类</Button>}>
+      <Table
+        columns={columns}
+        dataSource={this.state.categoryData}
+        bordered
+        pagination={{
+          showSizeChanger:true,
+          defaultPageSize:3,
+          pageSizeOptions:['3','6','9'],
+          showQuickJumper:true,
+        }}
+      />,
+    </Card>
   }
 }
